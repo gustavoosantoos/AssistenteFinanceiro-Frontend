3eafb08388529930a96562ad742284f66e754d11
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-01-18 16:32:36 -0200

    feat(layout): adição dos botões do menu superior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageHeaderComponent } from './layout/page-header/page-header.component';
-import { PageFooterComponent } from './layout/page-footer/page-footer.component';
 import { MenuSuperiorComponent } from './layout/menu-superior/menu-superior.component';
+import { PageFooterComponent } from './layout/page-footer/page-footer.component';
+import { PageHeaderComponent } from './layout/page-header/page-header.component';
+
 
 
 @NgModule({
@@ -20,7 +24,10 @@ import { MenuSuperiorComponent } from './layout/menu-superior/menu-superior.comp
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
