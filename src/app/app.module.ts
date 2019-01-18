7a5e619719099ecd09498784d5c73d18aa3b4bb4
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-01-18 13:38:57 -0200

    feat(layout): adição do footer da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './layout/page-header/page-header.component';
+import { PageFooterComponent } from './layout/page-footer/page-footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    PageFooterComponent
   ],
   imports: [
     BrowserModule,
