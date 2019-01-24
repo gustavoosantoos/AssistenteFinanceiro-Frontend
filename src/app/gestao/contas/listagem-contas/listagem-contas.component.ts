0a732141ce3e2589184acd0e0e1023e1573af4ca
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-01-24 09:38:47 -0200

    feat(contas): melhoria no alinhamento dos icones de conta

--- src/app/gestao/contas/listagem-contas/listagem-contas.component.ts
+++ src/app/gestao/contas/listagem-contas/listagem-contas.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ListagemContasComponent implements OnInit {
   public contas: Array<any> = [
     { nome: 'Conta Corrente', saldo: 132.0, saldoFinalMes: 0, icon: {color: '#767474', icon: 'attach_money'}},
-    { nome: 'Conta Poupança', saldo: 12.0, saldoFinalMes: 0, icon: {color: 'red', icon: 'attach_money'}},
-    { nome: 'Dinheiro na Carteira', saldo: 1030.0, saldoFinalMes: 0, icon: {color: 'black', icon: 'attach_money'}},
-    { nome: 'Teste', saldo: -123.0, saldoFinalMes: 1300, icon: {color: 'gray', icon: 'show_chart'}}
+    { nome: 'Conta Poupança', saldo: 12.0, saldoFinalMes: 0, icon: {color: 'rgb(153, 51, 204)', icon: 'attach_money'}},
+    { nome: 'Dinheiro na Carteira', saldo: 1030.0, saldoFinalMes: 0, icon: {color: 'rgb(36, 132, 122)', icon: 'attach_money'}},
+    { nome: 'Teste', saldo: -123.0, saldoFinalMes: 1300, icon: {color: 'rgb(194, 201, 0)', icon: 'show_chart'}}
   ];
 
   constructor() { }
