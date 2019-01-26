49342f43381535f04285a97dc1d98c2d1a1dee8f
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-01-26 17:46:36 -0200

    refactor(global): remoção de componentes não utilizados do template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,21 +11,6 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { FormsComponent } from './forms/forms.component';
-import { ButtonsComponent } from './buttons/buttons.component';
-import { TablesComponent } from './tables/tables.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { AlertsComponent } from './alerts/alerts.component';
-import { AccordionsComponent } from './accordions/accordions.component';
-import { BadgesComponent } from './badges/badges.component';
-import { ProgressbarComponent } from './progressbar/progressbar.component';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { PaginationComponent } from './pagination/pagination.component';
-import { DropdownComponent } from './dropdown/dropdown.component';
-import { TooltipsComponent } from './tooltips/tooltips.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
@@ -33,22 +18,7 @@ import { TabsComponent } from './tabs/tabs.component';
     NavbarComponent,
     SidebarComponent,
     FooterComponent,
-    DashboardComponent,
-    FormsComponent,
-    ButtonsComponent,
-    TablesComponent,
-    TypographyComponent,
-    IconsComponent,
-    AlertsComponent,
-    AccordionsComponent,
-    BadgesComponent,
-    ProgressbarComponent,
-    BreadcrumbsComponent,
-    PaginationComponent,
-    DropdownComponent,
-    TooltipsComponent,
-    CarouselComponent,
-    TabsComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
